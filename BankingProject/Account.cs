aa32bf50479327d017562b1d15445cb0fe7ac9b0
Philip Kerschner <pkerschner@zoominternet.net>
2022-02-07 12:00:56 -0500

    Add project files.

---
+++ BankingProject/Account.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking {
+    public class Account {
+        //Static Properties
+        private static int NextAccountNumber { get; set; } = 1;
+        // Instance Properties
+        public decimal Balance { get; set; }
+        public int AccountNumber { get; set; }
+        public string Description { get; set; }
+        // Methods
+        public bool Deposit(decimal Amount) {
+            if(Amount <= 0) {
+                throw new Exception("Amount must be positive.");            
+            }
+            Balance = Balance + Amount;
+            return true;
+        }     
+                public bool Withdraw(decimal Amount) {
+            if (Amount <= 0) {
+                throw new Exception("Amount must be positive.");
+            }
+            Balance = Balance - Amount;
+            return true;
+        }
+        public bool Transfer(decimal Amount, Account ToAccount) {
+            try {
+                Withdraw(Amount);
+            }catch (Exception) {
+                throw new Exception("Withdraw failed in Transfer");
+            }
+            ToAccount.Deposit(Amount);
+            return true;
+        }
+        public void Debug() {
+            Console.WriteLine($"{AccountNumber}|{Description}|{Balance:c}");
+        }
+        // Constructors
+        public Account() { //default constructor
+            AccountNumber = NextAccountNumber;
+            NextAccountNumber++;
+            Description = "New Account";
+            Balance = 0;
+        }
+
+    }
+}
---
+++ BankingProject/Program.cs
@@ -0,0 +1,27 @@
+﻿using Banking;
+using System;
+
+namespace BankingProject {
+    class Program {
+        static void Main(string[] args) {
+
+            Account acct1 = new Account();
+            acct1.Deposit(500);
+            acct1.Withdraw(200);
+            acct1.Debug();
+            //acct1.Withdraw(600);
+            //acct1.Debug();
+
+            Account acct2 = new Account();
+            acct2.Description = "My 2nd Account";
+            acct2.Deposit(1000);
+            acct2.Withdraw(350);
+            acct2.Debug();
+
+            acct2.Transfer(100, acct1);
+            acct1.Debug();
+            acct2.Debug();
+
+        }
+    }
+}
