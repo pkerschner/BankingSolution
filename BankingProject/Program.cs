656deb91a806329907dfcd6c58f47aff6948c385
Philip Kerschner <pkerschner@zoominternet.net>
2022-02-07 16:38:54 -0500

    Revision

--- BankingProject/Program.cs
+++ BankingProject/Program.cs
@@ -1,26 +1,34 @@
 ﻿using Banking;
+
 using System;
 
 namespace BankingProject {
     class Program {
         static void Main(string[] args) {
 
-            Account acct1 = new Account();
-            acct1.Deposit(500);
-            acct1.Withdraw(200);
-            acct1.Debug();
-            //acct1.Withdraw(600);
+            Savings sav1 = new Savings();
+            sav1.Deposit(100);
+            sav1.CalculateAndPayInterest(1);
+            sav1.CalculateAndPayInterest(1);
+            sav1.Debug();
+
+
+            //Account acct1 = new Account();
+            //acct1.Deposit(500);
+            //acct1.Withdraw(200);
             //acct1.Debug();
+            ////acct1.Withdraw(600);
+            ////acct1.Debug();
 
-            Account acct2 = new Account();
-            acct2.Description = "My 2nd Account";
-            acct2.Deposit(1000);
-            acct2.Withdraw(350);
-            acct2.Debug();
+            //Account acct2 = new Account();
+            //acct2.Description = "My 2nd Account";
+            //acct2.Deposit(1000);
+            //acct2.Withdraw(350);
+            //acct2.Debug();
 
-            acct2.Transfer(100, acct1);
-            acct1.Debug();
-            acct2.Debug();
+            //acct2.Transfer(100, acct1);
+            //acct1.Debug();
+            //acct2.Debug();
 
         }
     }
---
+++ BankingProject/Savings.cs
@@ -0,0 +1,19 @@
+﻿using Banking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingProject {
+
+    public class Savings : Account {
+
+        public decimal InterestRate { get; set; } = 0.12m;
+
+        public void CalculateAndPayInterest(int months) {
+            decimal interest = Balance * (InterestRate / 12) * months;
+            Deposit(interest);
+        }
+    }
+}
